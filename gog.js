ed51978f93a0843f374af41120e3e3ce8b926bc8
John Pope <ace@halffastsolutions.com>
2011-08-08 07:49:48 +0000

    Adding layout and index view

--- gog.js
+++ gog.js
@@ -1,9 +1,13 @@
 var config = require("./config.js");
+
 var express = require("express");
 var app = express.createServer();
 
+app.set("view engine", "html");
+app.register(".html", require("jqtpl").express);
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.listen(config.port, config.ip);
